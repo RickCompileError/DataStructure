60673e2433e689bc63d47dd889e7b1f7e798a5eb
CompileError <gngn15401@gmail.com>
2021-09-27 18:13:34 +0800

    fix heap sort bug

--- Sort/heap_sort.cpp
+++ Sort/heap_sort.cpp
@@ -3,16 +3,16 @@
 using namespace std;
 
 void print(int arr[], int size){
-    for (int i=0;i<size;i++) cout <<arr[i] <<' ';
+    for (int i=1;i<=size;i++) cout <<arr[i] <<' ';
     cout <<endl;
 }
 
 void siftdown(int arr[], int size, int ind){
     int parent=ind,child;
     bool stop=false;
-    while (parent*2<size && !stop){
-        if (parent*2+2<size && arr[parent*2+1]<arr[parent*2+2]) child=parent*2+2;
-        else child=parent*2+1;
+    while (parent*2<=size && !stop){
+        if (parent*2+1<=size && arr[parent*2]<arr[parent*2+1]) child=parent*2+1;
+        else child=parent*2;
         if (arr[parent]<arr[child]){
             swap(arr[parent],arr[child]);
             parent=child;
@@ -22,15 +22,15 @@ void siftdown(int arr[], int size, int ind){
 }
 
 void removeKey(int arr[], int size){
-    for (int i=size-1;i>=0;i--){
-        swap(arr[0],arr[i]);
-        siftdown(arr,i-1,0);
+    for (int i=size;i>=1;i--){
+        swap(arr[1],arr[i]);
+        siftdown(arr,i-1,1);
         // cout <<size-i <<". "; print(arr,size);
     }
 }
 
 void makeHeap(int arr[], int size){
-    for (int i=size/2;i>=0;i--){
+    for (int i=size/2;i>=1;i--){
         siftdown(arr,size,i);
     }
 }
@@ -47,7 +47,7 @@ int main()
 {
     srand(time(NULL));
     int sz=10;
-    int arr[sz];
-    for (int i=0;i<sz;i++) arr[i]=rand()%20;
+    int arr[sz+1];
+    for (int i=1;i<=sz;i++) arr[i]=rand()%20;
     sort(arr,sz);
 }
